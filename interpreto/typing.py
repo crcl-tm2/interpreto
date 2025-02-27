2d7f215baa410c0ac083054e2ac4f9898f404410
Antonin Poché <antonin.poche@irt-saintexupery.com>
2025-02-27 15:22:48 +0100

    small fix

--- interpreto/typing.py
+++ interpreto/typing.py
@@ -4,11 +4,11 @@ Generic type annotations for Interpreto
 
 from __future__ import annotations
 
-from typing import Any, Union
+from typing import Any, Optional, Union
 
 import torch
 
 TokenEmbedding = Any
 Activation = Any
 ModelInput = Any
-TensorBaseline = Union[torch.Tensor, float, int, type(None)]  # noqa: UP007
+TensorBaseline = Optional[Union[torch.Tensor, float, int]]  # noqa: UP007
