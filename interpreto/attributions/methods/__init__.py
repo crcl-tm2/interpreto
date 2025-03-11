7d69ec8c365692d64c173ae8f11913687a3ea96e
fanny-jourdan <fanny.jourdan@irt-saintexupery.com>
2025-03-11 16:08:56 +0100

    add attribution test

--- interpreto/attributions/methods/__init__.py
+++ interpreto/attributions/methods/__init__.py
@@ -1,3 +1,4 @@
 from .integrated_gradients import IntegratedGradients
+from .sobol_attribution import SobolAttribution
 
-__all__ = ["IntegratedGradients"]
+__all__ = ["IntegratedGradients", "SobolAttribution"]
