3a742d2cc7bb4439166c04497200e64716d780bc
Antonin Poché <antonin.poche@irt-saintexupery.com>
2025-04-16 18:48:19 +0200

    attribution aggregation: linear aggregation small fix

--- interpreto/attributions/aggregations/linear_regression_aggregation.py
+++ interpreto/attributions/aggregations/linear_regression_aggregation.py
@@ -39,17 +39,8 @@ from torch.nn import functional as F
 
 from interpreto.attributions.aggregations.base import Aggregator
 
-
-class DistancesFromMaskProtocol(Protocol):
-    """
-    Protocol for distance functions from masks
-    """
-
-    def __call__(self, mask: Float[torch.Tensor, "p l"]) -> Float[torch.Tensor, "l"]:  # noqa: UP037
-        """
-        Compute the distances from a mask
-        """
-        ...
+DistancesFromMaskProtocol = Callable[[Float[torch.Tensor, "p l"]], Float[torch.Tensor, "p"]]
+SimilarityKernelProtocol = Callable[[Float[torch.Tensor, "p"], float], Float[torch.Tensor, "p"]]
 
 
 def hamming_distance(mask: torch.Tensor) -> torch.Tensor:
@@ -120,10 +111,18 @@ class LinearRegressionAggregator(Aggregator):
 
     def __init__(
         self,
-        distance_function: DistancesFromMaskProtocol | None = None,
-        similarity_kernel: Callable = exponential_kernel,
+        distance_function: DistancesFromMaskProtocol | None = None,  # noqa: UP036
+        similarity_kernel: SimilarityKernelProtocol = exponential_kernel,
         kernel_width: float | Callable = default_kernel_width_fn,
     ):
+        """
+        Initialize the aggregator.
+
+        Args:
+            distance_function (DistancesFromMaskProtocol): distance function used to compute the similarity between perturbations and the original input from the mask.
+            similarity_kernel (Callable): similarity kernel used to compute the similarity between perturbations and the original input
+            kernel_width (float | Callable): kernel width used to compute the similarity between perturbations and the original input
+        """
         self.distance_function = distance_function
         self.similarity_kernel = similarity_kernel
         self.kernel_width = kernel_width
@@ -145,7 +144,7 @@ class LinearRegressionAggregator(Aggregator):
                 kernel_width: float = self.kernel_width
             weights: Float[torch.Tensor, "p"] = self.similarity_kernel(distances, kernel_width)  # noqa: UP037
         else:  # Kernel SHAP
-            weights: Float[torch.Tensor, "p"] = self.similarity_kernel(mask[:, 0], None)  # noqa: UP037
+            weights: Float[torch.Tensor, "p"] = self.similarity_kernel(mask[:, 0], 1.0)  # noqa: UP037
 
         # Compute closed form solution for the linear model
         # Add a constant column for the bias term
