d8f34acc9d1c31936c93bfe3262fcd908b3191fd
fanny-jourdan <fanny.jourdan@irt-saintexupery.com>
2025-07-28 17:35:08 +0200

    clean contribution shape

--- interpreto/attributions/base.py
+++ interpreto/attributions/base.py
@@ -396,19 +396,9 @@ class AttributionExplainer:
 
         # Aggregate the scores using the aggregator function and the perturbation masks.
         contributions = (
-            self.aggregator(score.detach(), mask.to(self.device) if mask is not None else None).squeeze(0)
+            self.aggregator(score.detach(), mask.to(self.device) if mask is not None else None)
             for score, mask in zip(scores, mask_generator, strict=True)
         )
-
-        # TODO: It is not super clean but I need this to keep the same dim in all cases (single class vs other)
-        clean_contributions = []
-        for contribution in contributions:
-            if contribution.dim() == 1:
-                # If the contribution is 1D, it means we have a single target or class. But we still need to keep the batch dimension.
-                clean_contributions.append(contribution.unsqueeze_(0))
-            else:
-                # If the contribution is 2D, it means we have multiple targets or classes.
-                clean_contributions.append(contribution)
 
         if self.use_gradient:
             granular_contributions = [
@@ -419,10 +409,10 @@ class AttributionExplainer:
                     inputs,  # type: ignore
                     self.tokenizer,
                 )
-                for contribution, inputs in zip(clean_contributions, model_inputs_to_explain, strict=True)
+                for contribution, inputs in zip(contributions, model_inputs_to_explain, strict=True)
             ]
         else:
-            granular_contributions = clean_contributions
+            granular_contributions = contributions
 
         # Decompose each input for the desired granularity level (tokens, words, sentences...)
         granular_inputs_texts: list[list[str]] = [
